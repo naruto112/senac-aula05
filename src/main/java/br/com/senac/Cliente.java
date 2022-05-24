7cc08a9c64f5a12c2e224fcd7ccf8ecf010f6a9a
Renato Moschetta <renatorock3@hotmail.com>
2022-05-24 20:46:48 -0300

    deployment aual05

---
+++ src/main/java/br/com/senac/Cliente.java
@@ -0,0 +1,35 @@
+package br.com.senac;
+
+public class Cliente {
+    private Number id;
+    private String nome;
+    private String email;
+    private Number telefone;    
+
+
+    public Number getId() {
+        return id;
+    }
+    public void setId(Number id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Number getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(Number telefone) {
+        this.telefone = telefone;
+    }
+
+}
---
+++ src/main/java/br/com/senac/GreetingResource.java
@@ -0,0 +1,51 @@
+package br.com.senac;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/cliente")
+public class GreetingResource {
+    Cliente cliente = new Cliente();
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Cliente get() {
+        return cliente;
+    }
+
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public void post(Cliente c) {
+        cliente.setId(c.getId());
+        cliente.setNome(c.getNome());
+        cliente.setEmail(c.getEmail());
+        cliente.setTelefone(c.getTelefone());
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    public Cliente put(Cliente c) {
+        cliente.setId(c.getId());
+        cliente.setNome(c.getNome());
+        cliente.setEmail(c.getEmail());
+        cliente.setTelefone(c.getTelefone());
+        return cliente;
+    }
+
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    public void delete() {
+        cliente.setId(null);
+        cliente.setNome(null);
+        cliente.setEmail(null);
+        cliente.setTelefone(null);
+    }
+
+}
